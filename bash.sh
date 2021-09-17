777d3dcb796697e587d9a7d11a898b8820ba0dbb
eyalrainit <eyalrainitz@gmail.com>
2021-09-17 13:36:03 +0300

    feature add

--- bash.sh
+++ bash.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
-PASSWORD="$@"
-ISOK=0
+PASSWORD="."
+OPTION=$1
+if [ $OPTION == "-f" ];
+then
+    if ! [ -f $2 ];
+    then
+        echo "This file doesn't exist" && exit 1;
+    fi
+PASSWORD=`cat $2`
+else
+ PASSWORD=$1
+ fi
+ISOK=0 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 if [ ${#PASSWORD} -lt 10 ]; then
-echo "minimun 10 cherecters" && ISOK=1
+echo -e "${RED}minimun 10 cherecters${NC}" && ISOK=1
 fi
 if ! [[  "$PASSWORD" =~ [[:upper:]] ]]; then
-  echo "uppercase character not found" && ISOK=1
+  echo -e "${RED}uppercase character not found${NC}" && ISOK=1
 fi
 if ! [[ $PASSWORD =~ [[:digit:]] ]]; then
-echo "password miss digit" && ISOK=1
+echo  -e "${RED}password miss digit${NC}" && ISOK=1
 fi
 if ! [[ $PASSWORD =~  [[:lower:]] ]]; then
-echo "password miss lowercase" && ISOK=1
+echo -e "${RED}password miss lowercase${NC}" && ISOK=1
 fi
 if [[ $PASSWORD =~ ['!@#$%^&*()_+'] ]];then
-  echo "It contains one of those !@#$%^&*()_+"
+  echo -e "${GREEN}It contains one of those !@#$%^&*()_+${NC}"
 fi
 if [ ISOK == 1 ]; then
-echo "password is not strong" && exit 1;
+echo -e "${RED}password is not strong${NC}" && exit 1;
 else 
-echo "password is ok" && exit 0;
+echo -e "${GREEN} password is ok${NC}" && exit 0;
 fi
